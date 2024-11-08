00b2751b67d276297e734f594fa9b82302a5b666
Myre36 <67600948+Myre36@users.noreply.github.com>
2024-11-08 10:37:04 +0100

    Did a thing

--- Assets/Scripts/KillPlayerScript.cs
+++ Assets/Scripts/KillPlayerScript.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class KillPlayerScript : MonoBehaviour
 {
     public Transform checkPoint;
+    public TMP_Text gravityText;
 
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.CompareTag("Player"))
         {
             collision.gameObject.transform.position = checkPoint.position;
+            gravityText.text = "You are falling down";
             collision.gameObject.GetComponent<GravityComponent>().gravityStatus = 0;
             collision.gameObject.GetComponent<PlayerMovement>().StartRotation();
             collision.gameObject.transform.rotation = checkPoint.rotation;
