a320d3b1d428e448b3353ef50ab28ea86c4690ea
Myre36 <67600948+Myre36@users.noreply.github.com>
2024-11-11 20:41:42 +0100

    Started relative gravity

--- Assets/Scripts/GravityButtonsScript.cs
+++ Assets/Scripts/GravityButtonsScript.cs
@@ -8,7 +8,42 @@ public class GravityButtonsScript : MonoBehaviour
 
     public GameObject player;
 
-    public void ChangeGravity()
+    public void ChangeGravityUp()
+    {
+        switch(player.GetComponent<PlayerMovement>().gravityNumber)
+        {
+            case 0:
+                rotationSetting = 1; 
+                break;
+            case 1:
+                rotationSetting = 0;
+                break;
+            case 2:
+                rotationSetting = 3;
+                break;
+            case 3:
+                rotationSetting = 2;
+                break;
+        }
+        player.GetComponent<PlayerMovement>().gravityNumber = rotationSetting;
+        player.GetComponent<LazerScript>().gravityType = rotationSetting;
+    }
+    public void ChangeGravityForward()
+    {
+        player.GetComponent<PlayerMovement>().gravityNumber = rotationSetting;
+        player.GetComponent<LazerScript>().gravityType = rotationSetting;
+    }
+    public void ChangeGravityBackwards()
+    {
+        player.GetComponent<PlayerMovement>().gravityNumber = rotationSetting;
+        player.GetComponent<LazerScript>().gravityType = rotationSetting;
+    }
+    public void ChangeGravityLeft()
+    {
+        player.GetComponent<PlayerMovement>().gravityNumber = rotationSetting;
+        player.GetComponent<LazerScript>().gravityType = rotationSetting;
+    }
+    public void ChangeGravityRight()
     {
         player.GetComponent<PlayerMovement>().gravityNumber = rotationSetting;
         player.GetComponent<LazerScript>().gravityType = rotationSetting;
