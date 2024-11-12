084ff278228fbe5651598b068acff26d2cd7dc50
Myre36 <67600948+Myre36@users.noreply.github.com>
2024-11-12 16:41:36 +0100

    Fixed the controlls

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -23,7 +23,7 @@ public class PlayerCamera : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButton(1))
         {
             gravityWheel.SetActive(true);
             Cursor.lockState = CursorLockMode.None;
