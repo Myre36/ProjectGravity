32d96997b39cd76472167227382d866f9835b119
Myre36 <67600948+Myre36@users.noreply.github.com>
2024-11-18 23:26:54 +0100

    Baked the lights again

--- Assets/Scripts/GravityButtonsScript.cs
+++ Assets/Scripts/GravityButtonsScript.cs
@@ -10,18 +10,27 @@ public class GravityButtonsScript : MonoBehaviour
 
     public GameObject highlight;
 
+    public Material originalMaterial;
+    public Material overrideMaterial;
+
+    public GameObject arrow;
+    public GameObject arrowHead;
+
     private void OnMouseOver()
     {
         highlight.SetActive(true);
+        arrow.GetComponent<Renderer>().material = overrideMaterial;
+        arrowHead.GetComponent<Renderer>().material = overrideMaterial;
         player.GetComponent<PlayerMovement>().gravityNumber = rotationSetting;
         player.GetComponent<LazerScript>().gravityType = rotationSetting;
         Debug.Log("Testing: This is with setting " + rotationSetting);
     }
-
     
     private void OnMouseExit()
     {
         highlight.SetActive(false);
+        arrow.GetComponent<Renderer>().material = originalMaterial;
+        arrowHead.GetComponent<Renderer>().material = originalMaterial;
     }
 
     
