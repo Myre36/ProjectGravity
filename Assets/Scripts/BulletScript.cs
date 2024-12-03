8ec06e924d8b03c60786f15881f99c000111b12a
Myre36 <67600948+Myre36@users.noreply.github.com>
2024-12-03 15:31:14 +0100

    Auto stash before merge of "main" and "origin/main"

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -6,11 +6,25 @@ public class BulletScript : MonoBehaviour
 {
     public int bulletType = 0;
 
+    public GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Interactable"))
         {
-            other.GetComponent<GravityComponent>().gravityStatus = bulletType;
+            if (other.gameObject.GetComponent<GravityComponent>().gravityStatus == bulletType)
+            {
+                other.gameObject.GetComponent<GravityComponent>().gravityStatus = player.GetComponent<GravityComponent>().gravityStatus;
+            }
+            else
+            {
+                other.GetComponent<GravityComponent>().gravityStatus = bulletType;
+            }
         }
 
         Destroy(gameObject);
